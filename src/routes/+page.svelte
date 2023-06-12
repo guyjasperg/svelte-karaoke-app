3c7eb48e05d6f1b5019441b23e45bce9dbc873a9
Guy Jasper Gonzaga <gg185018@ncr.com>
2023-06-12 17:58:08 +0800

    format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-
 //https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=ipanumpa%20kokaraoke&type=video&key=AIzaSyDXXlsxXYjEqZdsXUI2uLKCCGHFgMFV-7g
+
+<style lang="postcss">
+</style>
