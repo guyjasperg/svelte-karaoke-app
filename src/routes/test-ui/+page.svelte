52612f314730a89672db5d07779b2964029f1c50
Guy Jasper Gonzaga <gg185018@ncr.com>
2023-06-14 06:52:45 +0800

    update

---
+++ src/routes/test-ui/+page.svelte
@@ -0,0 +1,73 @@
+<!-- component -->
+
+<div class="h-screen w-screen flex flex-col gap-4 items-center justify-center bg-white">
+	<!-- Card 1 -->
+	<a href="#" class="w-[30rem] border-2 border-b-4 border-gray-200 rounded-xl hover:bg-gray-50">
+		<!-- Badge -->
+		<p class="bg-sky-500 w-fit px-4 py-1 text-sm font-bold text-white rounded-tl-lg rounded-br-xl">
+			FEATURED
+		</p>
+
+		<div class="grid grid-cols-6 p-5 gap-y-2">
+			<!-- Profile Picture -->
+			<div>
+				<img src="https://picsum.photos/seed/2/200/200" class="w-16 h-16 rounded-full" alt="" />
+			</div>
+
+			<!-- Description -->
+			<div class="col-span-5 md:col-span-4 ml-4">
+				<p class="text-sky-500 font-bold text-xs">20+ SPOTS LEFT</p>
+
+				<p class="text-gray-600 font-bold">[Intermediate/Advanced] Tea Time Conversation</p>
+
+				<p class="text-gray-400">Sat, Mar 12 . 7:00 - 8:30 AM</p>
+
+				<p class="text-gray-400">Beginner speakers</p>
+			</div>
+
+			<!-- Price -->
+			<div class="flex col-start-2 ml-4 md:col-start-auto md:ml-0 md:justify-end">
+				<p class="rounded-lg text-sky-500 font-bold bg-sky-100 py-1 px-3 text-sm w-fit h-fit">
+					$ 5
+				</p>
+			</div>
+		</div>
+	</a>
+
+	<!-- Card 2 -->
+	<a href="#" class="w-[30rem] border-2 border-b-4 border-gray-200 rounded-xl hover:bg-gray-50">
+		<!-- Badge -->
+		<p class="bg-sky-500 w-fit px-4 py-1 text-sm font-bold text-white rounded-tl-lg rounded-br-xl">
+			FEATURED
+		</p>
+
+		<div class="grid grid-cols-6 p-5 gap-y-2">
+			<!-- Profile Picture -->
+			<div>
+				<img
+					src="https://picsum.photos/seed/2/200/200"
+					class="w-16 h-16 rounded-full"
+					alt="loading"
+				/>
+			</div>
+
+			<!-- Description -->
+			<div class="col-span-5 md:col-span-4 ml-4">
+				<p class="text-sky-500 font-bold text-xs">20+ SPOTS LEFT</p>
+
+				<p class="text-gray-600 font-bold">[Intermediate/Advanced] Tea Time Conversation</p>
+
+				<p class="text-gray-400">Sat, Mar 12 . 7:00 - 8:30 AM</p>
+
+				<p class="text-gray-400">Beginner speakers</p>
+			</div>
+
+			<!-- Price -->
+			<div class="flex col-start-2 ml-4 md:col-start-auto md:ml-0 md:justify-end">
+				<p class="rounded-lg text-sky-500 font-bold bg-sky-100 py-1 px-3 text-sm w-fit h-fit">
+					$ 5
+				</p>
+			</div>
+		</div>
+	</a>
+</div>
