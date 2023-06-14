161ac34bb2f669d5c373545e003f5a5c73cb1a98
Guy Jasper Gonzaga <gg185018@ncr.com>
2023-06-14 11:41:11 +0800

    added search bar

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { searchSongs } from '../api/yt-api';
+	import Searchbar from '../../components/Searchbar.svelte';
+	import { comma } from 'postcss/lib/list';
 
 	// let ytVids = {};
 	let searchString = '';
+	let mounted = false;
+
+	onMount(() => {
+		console.log('onMount');
+		mounted = true;
+		ytV1.length = 0;
+	});
 
 	let ytV1 = [
 		{
@@ -19,42 +28,23 @@
 		}
 	];
 
-	onMount(() => {
-		ytV1 = [];
-	});
+	const handleSearch = async (event) => {
+		searchString = event.detail.text;
+		console.log('handleSearch', searchString);
 
-	const handleClick = async () => {
-		console.log('handleClick', searchString);
 		if (!searchString) return;
 
 		const result = await searchSongs(searchString);
-		// ytVids = result.items;
-		// console.log(ytVids);
 		ytV1 = result.items;
-		// console.log(ytV1);
 	};
 </script>
 
-<div class="w-screen py-5 flex justify-center px-5 border-b-2 border-gray-400">
-	<div>
-		<input
-			id="search"
-			type="text"
-			bind:value={searchString}
-			class="shadow appearance-none border rounded w-full p-2.5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-		/>
-	</div>
-	<div>
-		<button
-			class="text-xl bg-[darkcyan] text-[white] p-2.5 px-8 rounded-[10px] border-[none]"
-			on:click={handleClick}>Search</button
-		>
-	</div>
-</div>
+<Searchbar on:search={handleSearch} />
+
 <div class="rounded-sm bg-white mt-5 w-[100%] text-center items-center p-4 h-screen">
 	<h1 class="font-bold text-2xl">Search Result</h1>
-	<!-- {ytV1.length} -->
-	{#if ytV1.length > 0}
+	{#if ytV1.length > 0 && mounted}
+		{console.log('Inside if 2')}
 		{#each ytV1 as yt}
 			<div class="flex items-center text-center p-2 content-center flex-col">
 				<!-- svelte-ignore a11y-missing-attribute -->
@@ -64,19 +54,3 @@
 		{/each}
 	{/if}
 </div>
-
-<style>
-	/* Buttons */
-	.btn,
-	.btn-sm {
-		@apply font-medium inline-flex items-center justify-center border border-transparent rounded;
-	}
-
-	.btn {
-		@apply px-6 py-1 shadow-lg;
-	}
-	/* 
-	.btn-sm {
-		@apply px-4 py-2 shadow;
-	} */
-</style>
--- src/routes/test-ui/+page.svelte
+++ src/routes/test-ui/+page.svelte
@@ -1,8 +1,6 @@
 <!-- component -->
 
 <div class="w-1/3">
-	<form />
-
 	<label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
 		>Search</label
 	>
