8875d426aabfb7db87258c0cbdf5a021eb9245ac
guyjasperg <guyjasperg@gmail.com>
2023-06-16 11:09:16 +0800

    added youtube component

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,48 @@
 <script>
+	import YouTube from 'svelte-youtube';
+
+	const options = {
+		height: '390',
+		width: '640',
+		//  see https://developers.google.com/youtube/player_parameters
+		playerVars: {
+			autoplay: 0
+		}
+	};
+	let yt = null;
+	let sFunction = 'Play';
+	function onReady(event) {
+		console.log('onReady', event);
+		// event.detail.target.playVideo();
+		yt = event.detail.target;
+		// access to player in all event handlers via event.target
+		// event.target.pauseVideo();
+	}
+
+	const handlePlay = () => {
+		if (yt) {
+			if (sFunction === 'Play') {
+				sFunction = 'Pause';
+				yt.playVideo();
+			} else {
+				sFunction = 'Play';
+				yt.pauseVideo();
+			}
+		}
+		yt.playVideo();
+	};
+	const onEnd = () => {
+		console.log('onEnd');
+	};
+	const onStateChange = (event) => {
+		console.log('onStateChange', event);
+	};
 </script>
 
 <h1 class="text-2xl">About</h1>
 <p>Karaoke App v1.0</p>
 
-<div>
+<!-- <div>
 	<iframe
 		width="560"
 		height="315"
@@ -14,4 +52,13 @@
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 		allowfullscreen
 	/>
-</div>
+</div> -->
+
+<YouTube
+	videoId="2g811Eo7K8U"
+	{options}
+	on:ready={onReady}
+	on:end={onEnd}
+	on:stateChange={onStateChange}
+/>
+<button on:click={handlePlay}>{sFunction}</button>
